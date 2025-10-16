ca9cfce3e985db3665e81d5c37bd059946c838d1
Syed Rajib <syedrajib97@gmail.com>
2025-10-16 21:39:27 +0600

    Added Docker support and updated Flask config for Postgres connection

---
+++ backend/auth_service/app.py
@@ -0,0 +1,34 @@
+# app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+# .env লোড করা
+load_dotenv()
+
+app = Flask(__name__)
+
+# Configurations
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+# Extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+# একটি simple route
+@app.route('/')
+def hello():
+    return "Hello, Wenex!"
+
+# যদি তুমি সরাসরি python app.py চালাও
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
